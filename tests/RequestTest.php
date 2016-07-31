75fa15a045d8020d59d7500e34ebec21a33550a5
Zhuk Sergey <seregazhuk88@gmail.com>
2016-07-31 22:38:49 +0300

    fix: tests refactoring

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -3,9 +3,9 @@
 namespace seregazhuk\tests;
 
 use Mockery;
-use seregazhuk\SmsIntel\Contracts\HttpInterface;
 use seregazhuk\SmsIntel\Request;
 use seregazhuk\SmsIntel\XMLFormatter;
+use seregazhuk\SmsIntel\Contracts\HttpInterface;
 
 class RequestTest extends \PHPUnit_Framework_TestCase
 {
@@ -13,17 +13,17 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_adds_credentials_to_request_body()
     {
+        $requestParams =  [
+            'login'    => 'mylogin',
+            'password' => 'mypassword',
+            'key'      => 'value',
+        ];
+        $requestXML = $this->createParamsXml($requestParams);
+
         $httpClient = Mockery::mock(HttpInterface::class);
         $httpClient
             ->shouldReceive('post')
-            ->with(
-                Request::BASE_URL . 'send.php',
-                $this->createParamsXml([
-                    'login'    => 'mylogin',
-                    'password' => 'mypassword',
-                    'key'      => 'value',
-                ])
-            );
+            ->with(Request::BASE_URL . 'send.php', $requestXML);
 
         $request = new Request($httpClient, 'mylogin', 'mypassword');
         $request->exec('send', ['key' => 'value']);
@@ -32,13 +32,11 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_gets_api_endpoint_from_action()
     {
+        $requestXML = $this->createParamsXml(['login' => 'test', 'password' => 'test']);
         $httpClient = Mockery::mock(HttpInterface::class);
         $httpClient
             ->shouldReceive('post')
-            ->with(
-                Request::BASE_URL . 'send.php',
-                $this->createParamsXml(['login' => 'test', 'password' => 'test'])
-            );
+            ->with(Request::BASE_URL . 'send.php', $requestXML);
 
         $request = new Request($httpClient, 'test', 'test');
         $request->exec('send', []);
