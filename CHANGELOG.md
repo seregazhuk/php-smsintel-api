8cec87c4625b9fca6b7ee72fea50ce8e28aa9f92
Zhuk Sergey <seregazhuk88@gmail.com>
2017-05-26 06:58:57 +0300

    upd: php version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
-## v0.1.1 - 2017-05-25
+## v0.2.0 - 2017-05-25
 ### Updated:
  - Refactoring
+ - Requires PHP v5.6
--- tests/SmsIntelTest.php
+++ tests/SmsIntelTest.php
@@ -3,14 +3,14 @@
 namespace seregazhuk\tests;
 
 use seregazhuk\SmsIntel\SmsIntel;
-use seregazhuk\SmsIntel\Api\Sender;
+use seregazhuk\SmsIntel\Api\Requests\RequestsContainer;
 
 class SmsIntelTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
-    public function it_creates_an_instance_of_sender()
+    public function it_creates_an_instance_of_requests_container()
     {
-        $this->assertInstanceOf(Sender::class, SmsIntel::create('login', 'password'));
+        $this->assertInstanceOf(RequestsContainer::class, SmsIntel::create('login', 'password'));
     }
 
     /**
